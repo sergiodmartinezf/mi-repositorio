f5708a94973711e5056ba5ca24dbd88c5f95a1ff
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-09-13 17:37:23 -0300

    clase programacion

---
+++ Tareas/13-09-23-clase-progamacion/prompt-confirm
@@ -0,0 +1,6 @@
+
+let edad2 = prompt("ingresa edad: ", 0); //segundo parametro debe ir para no crashear algunos navegadores
+alert(`tienes ${edad2} años`);
+
+let question = confirm("¿cómo andas?");
+alert(question);
--- Tareas/13-09-23-clase-progamacion/tipos-de-datos.js
+++ Tareas/13-09-23-clase-progamacion/tipos-de-datos.js
@@ -14,4 +14,38 @@ let string2 = `holahola: ${string1}`;
 console.log(string1);
 console.log(string2);
 
+let v = true;
+let f = false;
+let truofake = true;
 
+if (truofake) {
+    console.log("es tru");
+} else {
+    console.log("es fake")
+}
+
+let indefinido;
+console.log(indefinido);
+
+//en python era dict() para diccionarios. aqui es object()
+//estas 2 formas son legales (la de llaves es mas rapida por escribir menos)
+let dic1 = new Object();
+let dic2 = {};
+
+let dic3 = {
+    palabra1: "hola",
+    palabra2: "mundo"
+}
+
+//imprimo diccionario
+console.log(dic3);
+
+//modifico y añado atributos
+dic3.palabra1 = "adios";
+dic3.palabra3 = "lol";
+
+console.log(typeof bigint);
+
+bigint = String(bigint);
+
+console.log(typeof bigint);
