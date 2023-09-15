903823d8c2d28a3fd8d8ddb20e776829296fae17
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-09-15 13:03:34 -0300

    Reto 1 Completado

---
+++ Retos/reto-2/javascript/reto2.js
@@ -0,0 +1,9 @@
+
+let nota1 = prompt("Ingrese la primera nota: ", 0)
+
+if (num_par_impar % 2 == 0) {
+    alert("El número es par")
+} else { 
+    alert("El número es impar")
+};
+
--- Retos/reto-2/reto2.js
+++
@@ -1,9 +0,0 @@
-
-let num_par_impar = prompt("Ingrese un número: ", 0); //segundo parametro debe ir para no crashear algunos navegadores
-
-if (num_par_impar % 2 == 0) {
-    alert("El número es par")
-} else { 
-    alert("El número es impar")
-};
-
