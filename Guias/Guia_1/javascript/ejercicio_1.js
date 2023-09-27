4a6a9b12d889078543626d8cb2931117d99f053c
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-09-27 14:33:25 -0300

    Guia 1 Ejercicio 1 listo

--- Guias/Guia_1/javascript/ejercicio_1.js
+++ Guias/Guia_1/javascript/ejercicio_1.js
@@ -1,6 +1,25 @@
-// Obtención de la primera nota
+// Recibe el número entero positivo.
 var num = prompt("Ingrese un número entero positivo: ", 0);
-while (num%1 != 0 && num < 0) {
-    var num = prompt("Ingrese un número entero positivo: ", 0);
+
+// Transforma el dato tipo string a un dato tipo número.
+num = Number(num);
+
+// Se verifica que no se acepte el dato ingresado mientras no sea un entero positivo.
+while (Number.isInteger(num) == false || num < 0) {
+    num = prompt("Ingrese un número entero positivo: ", 0);
+    num = Number(num);
 };
+
+// Se inicializa la variable que tendrá la suma total de los elementos pares hasta el entero ingresado.
+var suma = 0;
+
+// Se realiza la suma.
+for (var i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+        suma = suma + i;
+    };
+};
+
+// Se muestra la suma total.
+alert("La suma total de todos los números pares desde 1 hasta el entero ingresado es igual a: " + suma);
 
---
+++ Guias/Guia_1/javascript/ejercicio_2.js
@@ -0,0 +1,8 @@
+// Recibe la oración.
+var oracion = prompt("Ingrese una oración: ", 0);
+
+
+
+
+
+
