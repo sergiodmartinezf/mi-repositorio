e19499b7dab075ea2bea2159796387b8a0259314
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-09-26 23:45:48 -0300

    Guia 1 en proceso

---
+++ Guias/Guia_1/javascript/ejercicio_1.js
@@ -0,0 +1,6 @@
+// Obtención de la primera nota
+var num = prompt("Ingrese un número entero positivo: ", 0);
+while (num%1 != 0 && num < 0) {
+    var num = prompt("Ingrese un número entero positivo: ", 0);
+};
+
---
+++ Personal/javascript/estructuras_datos.js
@@ -0,0 +1,8 @@
+console.log("hola");
+console.log("munod");
+
+const miarr = ["sdf", "qwe", "rty", "ghj"];
+
+console.log(miarr);
+
+console.log(miarr[3]);
