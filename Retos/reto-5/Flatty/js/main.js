5d3785e86a590a8aa32c587be5ca934635ca0beb
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-10-13 12:39:12 -0300

    Reto 5

---
+++ Retos/reto-5/Flatty/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+
+  //Scroll para el menú y enlaces con la clase .scrollto
+  $('.smoothscroll').on('click', function (e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      let target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 48
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(".navbar-collapse a").on('click', function () {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+});
